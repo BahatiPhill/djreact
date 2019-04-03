42d82e23e511d3a1b13365eadc471ba49e8eadb3
Bahati <gur79tub@gmail.com>
2019-04-03 17:46:56 +0200

    Front: fecting data

--- backend/src/djreact/settings.py
+++ backend/src/djreact/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     
     'articles',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,3 +132,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
+
+
+CORS_ORIGIN_ALLOW_ALL = True
